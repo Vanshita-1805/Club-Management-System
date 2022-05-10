fb48222d7fa8437775126cb537034a1c0e8eaaf1
Vanshita-1805 <90504581+Vanshita-1805@users.noreply.github.com>
2022-05-10 14:56:41 +0530

    create class.py

---
+++ class.py
@@ -0,0 +1,27 @@
+#Creating Base class
+
+class club():
+  
+  #Club information
+  def clubinfo():
+    pass
+  
+  #Information of Club members
+  def memberinfo():
+    pass
+  
+  #Information of Events
+  def eventsinfo():
+    pass
+  
+  #Information of Funds
+  def fundsinfo():
+    pass
+  
+  
+  
+  #Child Class
+  
+  class Cultural(club):
+    
+    #In clubino
